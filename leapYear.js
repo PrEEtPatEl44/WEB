885c3b920af626a9eecede4a2a33c30c4b2640ee
Preet Patel <pbpatel48@myseneca.ca>
2023-12-22 12:50:38 -0500

    corrected leap year

--- leapYear.js
+++ leapYear.js
@@ -1,10 +1,7 @@
 var year=prompt("enter they year?");
-if(year%4===0){
-    if(year%100===0){
-        if(year%400===0){
-            console.log("is a leap year");        
-        }
-        console.log("not a leap year");
-    }
-    console.log("is a leap year");
+if(year%4!==0 || year%100===0 && year%400!=0){
+    console.log("not a leap year!");
+}
+else{
+    console.log("is a leap year!");
 }
