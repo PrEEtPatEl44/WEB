4d8239ffaf2f095881d9f776ed05dae6b33f8753
Preet Patel <pbpatel48@myseneca.ca>
2023-12-23 22:20:31 -0500

    update playerWins

--- DiceGame/app.js
+++ DiceGame/app.js
@@ -1,6 +1,14 @@
-window.onload= function(){
-    var n = (Math.floor(Math.random()*6))+1;
-    var img1=document.querySelector(".img1").setAttribute("src", "./images/dice"+n+".png");
-    var img2=document.querySelector(".img2").setAttribute("src", "./images/dice"+n+".png");
-   
-};
+
+    var n1 = (Math.floor(Math.random()*6))+1;
+    var n2 = (Math.floor(Math.random()*6))+1;
+    var img1=document.querySelector(".img1").setAttribute("src", "./images/dice"+n1+".png");
+    var img2=document.querySelector(".img2").setAttribute("src", "./images/dice"+n2+".png");
+   if(n1>n2){
+        document.querySelector("h1").innerHTML="Player 1 wins";
+   }
+   else if(n2>n1){
+    document.querySelector("h1").innerHTML="Player 2 wins";    
+   }
+   else{
+    document.querySelector("h1").innerHTML="Tie";
+   };
